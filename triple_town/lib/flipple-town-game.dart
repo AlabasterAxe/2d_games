edb7f56e4b7f723502226d5479fb8971064714ff
Matthew Keller <matthew.ed.keller@gmail.com>
2023-02-20 10:30:04 -0500

    add a little extra goodness

--- triple_town/lib/flipple-town-game.dart
+++ triple_town/lib/flipple-town-game.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flame/components.dart' show Anchor, Vector2;
 import 'package:flame/experimental.dart'
@@ -8,15 +9,27 @@ import 'package:triple_town/world/board.dart';
 
 class FlippleTownGame extends FlameGame with HasTappableComponents {
   var worldState = WorldState();
+  CameraComponent? cameraComponent;
+  var time = 0.0;
   @override
   FutureOr<void> onLoad() async {
     late World world;
     world = FlippleWorld(WorldState());
     add(world);
-    final camera = CameraComponent(world: world)
+    cameraComponent = CameraComponent(world: world)
       ..viewfinder.visibleGameSize = Vector2(BOARD_SIZE * 1.15, BOARD_SIZE * 2)
       ..viewfinder.position = Vector2(0, 0)
       ..viewfinder.anchor = Anchor.center;
-    add(camera);
+    add(cameraComponent!);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    time += dt;
+    cameraComponent!.viewfinder.position = Vector2(
+      (1 + 100 * sin(time)),
+      (1 + 100 * cos(time)),
+    );
   }
 }
